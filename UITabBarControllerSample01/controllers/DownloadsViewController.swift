65bd636393a5a00cee0d9cc5954de657c295d0f3
shigeki.yamamoto <shigeki.yamamoto@b-attraction.com>
2019-04-28 14:17:08 +0900

    update

---
+++ UITabBarControllerSample01/controllers/DownloadsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DownloadsViewController.swift
+//  UITabBarControllerSample01
+//
+//  Created by shigeki yamamoto on 2019/04/28.
+//  Copyright © 2019 shigeki yamamoto. All rights reserved.
+//
+
+import UIKit
+
+class DownloadsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
